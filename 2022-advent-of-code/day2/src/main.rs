b32388c8212458ddf25ee25468d0163aec8b962b
Daniel Piper <piperdaniel1@gmail.com>
2022-12-07 00:41:18 -0800

    most overengineered thing ever

--- 2022-advent-of-code/day2/src/main.rs
+++ 2022-advent-of-code/day2/src/main.rs
@@ -1,52 +1,153 @@
 use std::{fs::File, io::Read};
+use std::fmt::Display;
+
+enum Strategy {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum GameResult {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Display for Strategy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Strategy::Rock => write!(f, "rock"),
+            Strategy::Paper => write!(f, "paper"),
+            Strategy::Scissors => write!(f, "scissors"),
+        }
+    }
+}
+
+fn string_to_strategy(s: &str) -> Strategy {
+    match s {
+        "A" => Strategy::Rock,
+        "B" => Strategy::Paper,
+        "C" => Strategy::Scissors,
+        "X" => Strategy::Rock,
+        "Y" => Strategy::Paper,
+        "Z" => Strategy::Scissors,
+        _ => panic!("Invalid strategy"),
+    }
+}
+
+trait ParseStrategy {
+    fn parse_strategy(&self) -> Option<Strategy>;
+}
+impl ParseStrategy for String {
+    fn parse_strategy(&self) -> Option<Strategy> {
+        Some(string_to_strategy(self))
+    }
+}
+impl ParseStrategy for &str {
+    fn parse_strategy(&self) -> Option<Strategy> {
+        Some(string_to_strategy(self))
+    }
+}
+impl ParseStrategy for Option<String> {
+    fn parse_strategy(&self) -> Option<Strategy> {
+        match self {
+            Some(s) => Some(string_to_strategy(s)),
+            None => None,
+        }
+    }
+}
+
+trait ParseResult {
+    fn parse_result(&self) -> GameResult;
+}
+impl ParseResult for String {
+    fn parse_result(&self) -> GameResult {
+        match self.as_str() {
+            "Z" => GameResult::Win,
+            "X" => GameResult::Lose,
+            "Y" => GameResult::Draw,
+            _ => panic!("Invalid result"),
+        }
+    }
+}
 
 struct Round {
-    pub p1_play: String,
-    pub p2_play: String,
+    pub p1_play: Option<Strategy>,
+    pub p2_play: Option<Strategy>,
+    pub result: GameResult,
 }
 
 impl Round {
-    pub fn new(p1_play: String, p2_play: String) -> Round {
+    pub fn new(p1_play: impl ParseStrategy, p2_play: impl ParseStrategy, exp_result: impl ParseResult) -> Round {
         Round {
-            p1_play,
-            p2_play,
+            p1_play: p1_play.parse_strategy(),
+            p2_play: p2_play.parse_strategy(),
+            result: exp_result.parse_result(),
         }
     }
 
     pub fn new_from_string(s: &str) -> Round {
         let mut split = s.split_whitespace();
-        Round::new(split.next().unwrap().to_string(), split.next().unwrap().to_string())
+        Round::new(split.next().unwrap().to_string(), None, split.next().unwrap().to_string())
     }
 
-    fn get_play_score(&self, play: &String) -> i32 {
-        match play.as_str() {
-            "A" => 1,
-            "B" => 2,
-            "C" => 3,
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            e => panic!("Invalid play: {}", e),
+    // Fills in the missing strategy for the round
+    // so that player two has the result r
+    pub fn fill_none_value(&mut self) {
+        match self.result {
+            GameResult::Win => {
+                if self.p1_play.is_none() {
+                    self.p1_play = Some(self.p2_play.as_ref().unwrap().get_winning_strategy());
+                }
+                if self.p2_play.is_none() {
+                    self.p2_play = Some(self.p1_play.as_ref().unwrap().get_losing_strategy());
+                }
+            }
+            GameResult::Lose => {
+                if self.p1_play.is_none() {
+                    self.p1_play = Some(self.p2_play.as_ref().unwrap().get_losing_strategy());
+                }
+                if self.p2_play.is_none() {
+                    self.p2_play = Some(self.p1_play.as_ref().unwrap().get_winning_strategy());
+                }
+            }
+            GameResult::Draw => {
+                if self.p1_play.is_none() {
+                    self.p1_play = Some(self.p2_play.as_ref().unwrap().get_draw_strategy());
+                }
+                if self.p2_play.is_none() {
+                    self.p2_play = Some(self.p1_play.as_ref().unwrap().get_draw_strategy());
+                }
+            }
+        }
+
+    }
+
+    fn get_play_score(&self, play: &Strategy) -> i32 {
+        match play {
+            Strategy::Rock => 1,
+            Strategy::Paper => 2,
+            Strategy::Scissors => 3,
         }
     }
 
     pub fn get_p2_score(&self) -> i32 {
         let mut score = 0;
 
-        score += self.get_play_score(&self.p2_play);
+        score += self.get_play_score(&self.p2_play.as_ref().unwrap());
 
         // Draw
-        if self.p2_play.draws(&self.p1_play) {
+        if self.p2_play.as_ref().unwrap().draws(&self.p1_play.as_ref().unwrap()) {
             score += 3;
         }
 
         // Wins
-        else if self.p2_play.beats(&self.p1_play) {
+        else if self.p2_play.as_ref().unwrap().beats(&self.p1_play.as_ref().unwrap()) {
             score += 6;
         }
 
         // Loses
-        else if self.p2_play.loses(&self.p1_play) {
+        else if self.p2_play.as_ref().unwrap().loses(&self.p1_play.as_ref().unwrap()) {
             score += 0;
         }
 
@@ -55,40 +156,103 @@ impl Round {
 }
 
 trait Beats {
-    fn beats(&self, other: &String) -> bool;
+    fn beats(&self, other: &Strategy) -> bool;
 }
 
 trait Draws {
-    fn draws(&self, other: &String) -> bool;
+    fn draws(&self, other: &Strategy) -> bool;
 }
 
 trait Loses {
-    fn loses(&self, other: &String) -> bool;
+    fn loses(&self, other: &Strategy) -> bool;
 }
 
-impl Beats for String {
-    fn beats(&self, other: &String) -> bool {
-        match self.as_str() {
-            "A" => other == "C",
-            "X" => other == "Z",
-            "B" => other == "A",
-            "Y" => other == "X",
-            "C" => other == "B",
-            "Z" => other == "Y",
-            _ => panic!("Invalid play"),
+trait StrategyExt {
+    fn get_losing_strategy(&self) -> Strategy;
+    fn get_winning_strategy(&self) -> Strategy;
+    fn get_draw_strategy(&self) -> Strategy;
+}
+
+impl Beats for Strategy {
+    fn beats(&self, other: &Strategy) -> bool {
+        match self {
+            Strategy::Rock => match other {
+                Strategy::Scissors => true,
+                _ => false,
+            },
+            Strategy::Paper => match other {
+                Strategy::Rock => true,
+                _ => false,
+            },
+            Strategy::Scissors => match other {
+                Strategy::Paper => true,
+                _ => false,
+            },
         }
     }
 }
 
-impl Draws for String {
-    fn draws(&self, other: &String) -> bool {
-        self == other
+impl Draws for Strategy {
+    fn draws(&self, other: &Strategy) -> bool {
+        match self {
+            Strategy::Rock => match other {
+                Strategy::Rock => true,
+                _ => false,
+            },
+            Strategy::Paper => match other {
+                Strategy::Paper => true,
+                _ => false,
+            },
+            Strategy::Scissors => match other {
+                Strategy::Scissors => true,
+                _ => false,
+            },
+        }
     }
 }
 
-impl Loses for String {
-    fn loses(&self, other: &String) -> bool {
-        !self.beats(other) && !self.draws(other)
+impl Loses for Strategy {
+    fn loses(&self, other: &Strategy) -> bool {
+        match self {
+            Strategy::Rock => match other {
+                Strategy::Paper => true,
+                _ => false,
+            },
+            Strategy::Paper => match other {
+                Strategy::Scissors => true,
+                _ => false,
+            },
+            Strategy::Scissors => match other {
+                Strategy::Rock => true,
+                _ => false,
+            },
+        }
+    }
+}
+
+impl StrategyExt for Strategy {
+    fn get_losing_strategy(&self) -> Strategy {
+        match self {
+            Strategy::Rock => Strategy::Paper,
+            Strategy::Paper => Strategy::Scissors,
+            Strategy::Scissors => Strategy::Rock,
+        }
+    }
+
+    fn get_winning_strategy(&self) -> Strategy {
+        match self {
+            Strategy::Rock => Strategy::Scissors,
+            Strategy::Paper => Strategy::Rock,
+            Strategy::Scissors => Strategy::Paper,
+        }
+    }
+
+    fn get_draw_strategy(&self) -> Strategy {
+        match self {
+            Strategy::Rock => Strategy::Rock,
+            Strategy::Paper => Strategy::Paper,
+            Strategy::Scissors => Strategy::Scissors,
+        }
     }
 }
 
@@ -117,9 +281,11 @@ fn main() {
         .collect::<Vec<Round>>();
 
     let mut total_p2_score = 0;
-    for round in round_vec {
+    for mut round in round_vec {
+        round.fill_none_value();
+
         if debug {
-            println!("P1: {}, P2: {}", round.p1_play, round.p2_play);
+            println!("P1: {}, P2: {}", round.p1_play.as_ref().unwrap(), round.p2_play.as_ref().unwrap());
             println!("P2 score: {}", round.get_p2_score());
             println!("----------------");
         }
